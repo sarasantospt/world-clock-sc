c0ac09236dab99438defb03ab618fa2fbb3d6a05
sarasantospt <sarasantos95pt@gmail.com>
2023-07-03 22:26:06 +0100

    added .tz  to convert into time zone

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 function showDate(event) {
-  if (event.value.length> 0) {
-  let currentTime = moment().format("dddd, MMMM D, YYYY h:mm A");
+  if (event.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
 
-  alert(`It is ${currentTime} in ${event.target.value}`);
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
 
