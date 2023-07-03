b267f980e676fc7dc796d008a1957e0592b21b33
sarasantospt <sarasantos95pt@gmail.com>
2023-07-03 21:59:19 +0100

    Added a data and time format

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
-function showDate(event) {}
+function showDate(event) {
+  let currentTime = moment().format("dddd, MMMM D, YYYY h:mm A");
+}
 
 let selectElement = document.querySelector("#world-time");
 selectElement.addEventListener("change", showDate);
